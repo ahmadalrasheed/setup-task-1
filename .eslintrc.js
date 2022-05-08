daebfea89efcea1739bf0b2142e982b14329300f
emadNadsoft <85551534+emadNadsoft@users.noreply.github.com>
2022-05-08 13:39:26 +0300

    testing lint staged 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,60 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    "dot-notation": "off",
-    "arrow-body-style": "off",
-    "import/no-named-default": "off",
-    "import/prefer-default-export": "off",
-    "import/extensions": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".tsx", ".ts"] }],
-    camelcase: [2, { properties: "never" }],
-    "react/function-component-definition": [
-      2,
-      {
-        namedComponents: "function-declaration",
-      },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/strict',
     ],
-  },
-
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        moduleDirectory: ["src", "node_modules"],
-      },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-  },
+    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
+    rules: {
+        'react-hooks/exhaustive-deps': 'error',
+        'react/prop-types': 'off',
+        'no-var': 'error',
+        'brace-style': 'error',
+        'prefer-template': 'error',
+        radix: 'error',
+        'space-before-blocks': 'error',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+                args: 'none',
+                ignoreRestSiblings: true,
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: [
+                '*.ts',
+                '**/*.test.js',
+                '**/*.test.jsx',
+                '**/*.test.tsx',
+                '**/*.spec.js',
+                '**/*.spec.jsx',
+                '**/*.spec.tsx',
+            ],
+            rules: {
+                'no-undef': 'off',
+            },
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
