120cee252df9e90604324322cf3ad572993c32cf
emadNadsoft <85551534+emadNadsoft@users.noreply.github.com>
2022-05-10 11:17:34 +0300

    dasdasda

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,42 @@
-import { Button } from 'components/button';
+import { isEmpty, get, includes, find } from 'lodash';
+import * as pages from 'pages';
+import { DashboardLayout, AuthLayout } from 'layout';
+
+const pageRoutes = {
+    home: {
+        Page: pages.Home,
+        Layout: DashboardLayout,
+    },
+    signin: {
+        Page: pages.Signin,
+        Layout: AuthLayout,
+    },
+    '404': {
+        Page: pages.NotFound,
+        Layout: AuthLayout,
+    },
+    loading: {
+        Page: pages.Loading,
+        Layout: DashboardLayout,
+    },
+};
+
+const getPageStructure = (route: string) => {
+    if (isEmpty(route)) {
+        return 'loading...';
+    }
+
+    const routePaths = Object.keys(get(route, 'meta.params', {}));
+    const pageRoute =
+        find(routePaths.reverse(), (route: string) => includes(Object.keys(pageRoutes), route)) ||
+        '404';
+
+    return pageRoutes[pageRoute];
+};
+
 
 function App() {
-    return (
-        <div className="App">
-            <Button>asddasdasasdasd</Button>
-        </div>
-    );
+    return <div className="App">Home</div>;
 }
 
 export default App;
